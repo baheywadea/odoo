4662b403e340a22bb4b3a4a04ea73af5554166c1
macbook <bahey.wadea@gmail.com>
2024-04-16 03:27:47 +0300

    Add Website Form and update complaint models and update views and add website menu for complaint

--- bloopark_realestate/controllers/main.py
+++ bloopark_realestate/controllers/main.py
@@ -3,12 +3,14 @@
 
 import warnings
 from datetime import datetime, timedelta
-
+import logging
 from odoo import http, _
 from odoo.addons.http_routing.models.ir_http import slug
 from odoo.osv.expression import AND
 from odoo.http import request
 from odoo.tools.misc import groupby
+
+_logger = logging.getLogger(__name__)
 
 
 class WebsitePropertyComplaint(http.Controller):
@@ -24,3 +26,24 @@ class WebsitePropertyComplaint(http.Controller):
             'complaint_type_ids': complaint_type_ids,
         }
         return request.render("bloopark_realestate.apply_complaint",values)
+
+    @http.route('/property/complaint/submit', type='http', auth="public", website=True, methods=["POST"])
+    def complaint_apply_record(self, **kwargs):
+        _logger.warning("/property/complaint/submit"+str(kwargs))
+        partner_vals = {'name':kwargs.get('partner_name'),
+                        'email': kwargs.get('email_from'),
+                        'street': kwargs.get('customer_street'),
+                        'street2': kwargs.get('customer_street2'),
+                        'city': kwargs.get('customer_city'),
+                        }
+
+        partner_id = request.env['res.partner'].sudo().create(partner_vals)
+        complaint_type = request.env['complaint.type'].sudo().search([('name','=',kwargs.get('type'))])
+        complaint_vals = {'partner_id':partner_id.id,
+                          'customer_house_no': kwargs.get('customer_house_no'),
+                          'customer_Flat_no': kwargs.get('customer_Flat_no'),
+                          'customer_description': kwargs.get('description'),
+                          'type':complaint_type[0].id
+                          }
+        complaint_id = request.env['property.complaint'].sudo().create(complaint_vals)
+        return request.redirect("/complaint-thank-you?id="+str(complaint_id.id))
